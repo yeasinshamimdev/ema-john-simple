a7e2bf978cab162cfa3f01d8d8bbb5bd4837a015
Shamim <yeasinshamim7@gmail.com>
2022-03-22 23:46:25 +0600

    order summary done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({ cart }) => {
+    console.log(cart);
+
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 10 / 100).toFixed(2);
+
+    const grandTotal = total + shipping + parseFloat(tax);
     return (
-        <div>
-            <h3>order summary </h3>
-            <p>cart length: {cart.length}</p>
+        <div className='cart'>
+            <h3 className='order-summary'>Order Summary </h3>
+            <p>Product Added: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
