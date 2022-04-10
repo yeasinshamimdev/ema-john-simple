b99aae4f219a2e0373f0640b56ca8fe28080f14b
Shamim <yeasinshamim7@gmail.com>
2022-04-11 05:17:45 +0600

    signout implements

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -17,7 +18,11 @@ function App() {
         <Route path='/' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Order></Order>} ></Route>
-        <Route path='/inventory' element={<Inventory></Inventory>} ></Route>
+        <Route path='/inventory' element={
+          <RequireAuth>
+            <Inventory></Inventory>
+          </RequireAuth>
+        } ></Route>
         <Route path='/about' element={<About></About>} ></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import './Login.css';
 
@@ -13,7 +13,10 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+
     const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
 
     const handleEmailBlur = event => {
         setEmail(event.target.value);
@@ -28,7 +31,7 @@ const Login = () => {
         signInWithEmailAndPassword(email, password);
     }
     if (user) {
-        navigate('/shop');
+        navigate(from, { replace: true });
     }
     return (
         <div className='form-container'>
